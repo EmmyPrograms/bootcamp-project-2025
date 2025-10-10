ca5e0ad2b6464e5b98db6e57887eba87c24bd1f1
Emmy <ameliaharris492@gmail.com>
2025-10-10 10:43:49 -0700

    build: got website prepped for typescript; additionally, implemented first steps in blog like blog.ts

---
+++ src/blog.ts
@@ -0,0 +1,28 @@
+type Blog = {
+    title: string;
+    date: string;
+    description: string;
+    image: string;
+    imageAlt: string;
+    slug: string;
+
+}
+
+const blogs: Blog[] = [
+    {
+        title: "First Blog Post",
+        date: "10/10/2025",
+        description: "This is my first blog post",
+        image: "nothing rn",
+        imageAlt: "No image yet",
+        slug: "slug",
+    },
+    {
+        title: "Second Blog Post",
+        date: "XX/XX/XXXX",
+        description: "N/A",
+        image: "N/A",
+        imageAlt: "N/A",
+        slug: "slug",
+    }
+];
