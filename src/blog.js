d0440037fb63d79f67ab56dff8ef26611dddc0a5
Emmy <ameliaharris492@gmail.com>
2025-10-13 13:42:14 -0700

    feat: implemented automatic blog content creation in blogs.html with blog.ts; additionally, added one blog jumpstart, not yet styled.

--- src/blog.js
+++ src/blog.js
@@ -1,11 +1,11 @@
 var Blogs = [
     {
-        title: "First Blog Post",
-        date: "10/10/2025",
-        description: "This is my first blog post",
-        image: "nothing rn",
-        imageAlt: "No image yet",
-        slug: "slug",
+        title: "JumpStart Program",
+        date: "10/12/2025",
+        description: "A program offered to incoming transfers to curve the transfer shock phenomenon.",
+        image: "./images/Jumpstart.jpg",
+        imageAlt: "Jumpstart Program",
+        slug: "jumpstart",
     },
     {
         title: "Second Blog Post",
@@ -16,19 +16,28 @@ var Blogs = [
         slug: "slug",
     },
 ];
-var blogContainer = document.getElementById("blog");
+var blogContainer = document.getElementById("blog-content");
+console.log(blogContainer);
 Blogs.forEach(function (blog) {
     var div = document.createElement("div");
     div.className = "blog-post";
     var image = document.createElement("img");
     image.src = blog.image;
     image.alt = blog.imageAlt;
+    image.width = 650;
+    image.height = 300;
     var h1 = document.createElement("h1");
-    h1.textContent = blog.title;
+    var a = document.createElement("a");
+    a.textContent = blog.title;
+    a.href = "/blogs/".concat(blog.slug, ".html");
+    h1.appendChild(a);
+    var h2 = document.createElement("h2");
+    h2.textContent = blog.date;
     var p = document.createElement("p");
     p.textContent = blog.description;
     div.appendChild(image);
     div.appendChild(h1);
+    div.appendChild(h2);
     div.appendChild(p);
     if (blogContainer)
         blogContainer.appendChild(div);
