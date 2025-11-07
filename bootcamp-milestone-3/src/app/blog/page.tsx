9051e7cd8b7f1d1b8d8949c69e586610e54c61f2
Emmy <ameliaharris492@gmail.com>
2025-11-07 10:03:04 -0800

    feat: implemented the database; need to figure out URI issue

--- bootcamp-milestone-3/src/app/blog/page.tsx
+++ bootcamp-milestone-3/src/app/blog/page.tsx
@@ -5,9 +5,25 @@
 */
 
 import BlogPreview from "@/components/blogPreview";
-import blogs from "@/app/blogData";
+//import blogs from "@/app/blogData";
+import connectDB from "@/database/db";
+import Blog from "@/database/blogSchema";
 
-export default function BlogPage() {
+async function getBlogs(){
+	await connectDB() // function from db.ts before
+
+	try {
+			// query for all blogs and sort by date
+	    const blogs = await Blog.find().sort({ date: -1 }).orFail()
+			// send a response as the blogs as the message
+	    return blogs
+	} catch (err) {
+	    return null
+	}
+}
+
+export default async function BlogPage() {
+  const blogs = await getBlogs();
   return (
     <div className=" flex flex-col items-center">
       <title>Blog</title>
@@ -15,17 +31,21 @@ export default function BlogPage() {
         <u>Blogs</u>
       </h1>
       <div id="blog-content" className="gap-8 px-6 py-12 text-secondary">
-        {blogs.map((blog) => (
-          <BlogPreview
-            key = {blog.title}
-            title={blog.title}
-            description={blog.description}
-            image={blog.image}
-            date={blog.date}
-            imageAlt={blog.imageAlt}
-            slug={blog.slug}
-          />
-        ))}
+        {blogs && blogs.length > 0 ? (
+          blogs.map((blog) => (
+            <BlogPreview
+              key={blog._id}
+              title={blog.title}
+              description={blog.description}
+              image={blog.image}
+              date={blog.date}
+              imageAlt={blog.imageAlt}
+              slug={blog.slug}
+            />
+          ))
+        ) : (
+          <p>No blogs found.</p>
+        )}
       </div>
       <footer className="footer">
         © 2025 Amelia Harris | All Rights Reserved
@@ -33,3 +53,5 @@ export default function BlogPage() {
     </div>
   );
 }
+
+
