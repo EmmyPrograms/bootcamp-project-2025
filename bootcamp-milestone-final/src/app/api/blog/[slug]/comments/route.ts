8b025a45aef155b4d33ea19ac460afa4349360fb
Emmy <ameliaharris492@gmail.com>
2025-12-01 17:27:25 -0800

    feat: implemented api endpoint for blog comments and successfully tested with postman

---
+++ bootcamp-milestone-final/src/app/api/blog/[slug]/comments/route.ts
@@ -0,0 +1,41 @@
+import { NextRequest, NextResponse } from "next/server";
+import connectDB from "@/database/db";
+import Blog from "@/database/blogSchema";
+
+type IParams = {
+  params: Promise<{ slug: string }>;
+};
+
+export async function POST(req: NextRequest, { params }: IParams) {
+  const { slug } = await params;
+  await connectDB();
+  const body = await req.json();
+  if (!body) {
+    return NextResponse.json(
+      { error: "Invalid comment data" },
+      { status: 400 }
+    );
+  }
+  if (!body.user || !body.comment) {
+    return NextResponse.json(
+      { error: "Missing user or comment" },
+      { status: 400 }
+    );
+  }
+
+  const newComment = {
+    user: body.user,
+    comment: body.comment,
+    time: new Date(),
+  };
+
+  const updatedBlog = await Blog.findOneAndUpdate(
+    { slug },
+    { $push: { comments: newComment } },
+    { new: true }
+  );
+  if (!updatedBlog) {
+    return NextResponse.json({ error: "Blog not found" }, { status: 404 });
+  }
+  return NextResponse.json(newComment);
+}
